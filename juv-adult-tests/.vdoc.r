d7e2a9675152ec6c52d8823ccc59e29f5fa47fb2
Sophia Rutman <srutman02@gmail.com>
2024-12-03 14:57:37 -0500

    more tests

--- juv-adult-tests/.vdoc.r
+++ juv-adult-tests/.vdoc.r
@@ -8,14 +8,15 @@
 #
 library(tidyverse)
 library(dplyr)
+library(DescTools)
+
 #
 #
 #
 #
-adult <- read_csv("../2013-2023/arrests_2013-2023.csv")
+adult <- read_csv("../2013-2023/arrests_2013-2023.csv") |> filter(Arrest.Year >= 2016)
 adult$Arrest.Date <- as.Date(adult$Arrest.Date, format = "%Y-%m-%d")
 grouped_adult <- adult |> group_by(Arrest.Date) |> summarize(count = n())
-grouped_adult <- head(grouped_adult, -1) 
 grouped_adult$count <- grouped_adult$count / sum(grouped_adult$count)
 grouped_adult
 # last date has absurd amount of arrests, on the scale of 10^8
@@ -36,29 +37,32 @@ ks.test(grouped_adult$count, grouped_juv$count)
 #
 #
 #
-grouped.adult <- adult |> group_by(Arrest.Category) |> summarize(count = n()) |> arrange(desc(count))
-grouped.adult$count <- grouped.adult$count / sum(grouped.adult$count)
-sliced <- grouped.adult |> slice(0:10)
+grouped.adult.unscaled <- adult |> group_by(Arrest.Category) |> summarize(count = n()) |> arrange(desc(count))
+grouped.adult$count <- grouped.adult.unscaled$count / sum(grouped.adult.unscaled$count)
+sliced <- grouped.adult |> slice(0:20)
 
-par(mar = c(15, 4, 4, 4))
-barplot(height = sliced$count, names.arg = sliced$Arrest.Category, las = 2)
+par(mar = c(30, 4, 4, 4))
+barplot(height = sliced$count, names.arg = sliced$Arrest.Category, las = 2, main = "Adult Arrests")
 
-grouped.juv <- juv |> group_by(TOP_CHARGE_DESCRIPTION) |> summarize(count = n()) |> arrange(desc(count))
-grouped.juv$count <- grouped.juv$count / sum(grouped.juv$count)
-sliced <- grouped.juv |> slice(0:10)
+grouped.juv.unscaled <- juv |> group_by(TOP_CHARGE_DESCRIPTION) |> summarize(count = n()) |> arrange(desc(count))
+grouped.juv$count <- grouped.juv.unscaled$count / sum(grouped.juv.unscaled$count)
+sliced <- grouped.juv |> slice(0:20)
 
-barplot(height = sliced$count, names.arg = sliced$TOP_CHARGE_DESCRIPTION, las = 2)
+#barplot(height = sliced$count, names.arg = sliced$TOP_CHARGE_DESCRIPTION, las = 2, main = "Juvenile Arrests")
 
 names(grouped.juv) <- c("Arrest.Category", "count.juv")
-combined <- full_join(grouped.adult, grouped.juv, by = "Arrest.Category")
+names(grouped.juv.unscaled) <- c("Arrest.Category", "count.juv")
+
+combined <- full_join(grouped.adult.unscaled, grouped.juv.unscaled, by = "Arrest.Category")
 
 combined$count.juv <- as.numeric(combined$count.juv)
 combined$count <- as.numeric(combined$count)
 combined$count.juv[is.na(combined$count.juv)] <- 0
 combined$count[is.na(combined$count)] <- 0
 combined <- data.frame(Adult = combined$count, Juv = combined$count.juv)
-combined <- combined * 1000000
-chisq.test(combined)
+combined
+GTest(combined)
+#
 #
 #
 #
@@ -66,18 +70,26 @@ chisq.test(combined)
 grouped.adult <- adult |> group_by(Arrest.Location.PSA) |> summarise(count = n())
 grouped.adult$count <- grouped.adult$count / sum(grouped.adult$count)
 grouped.adult$count[is.na(grouped.adult$count)] <- 0
+grouped_adult <- grouped.adult[!is.na(grouped.adult$Arrest.Location.PSA),]
 
 grouped.juv <- juv |> group_by(CRIME_PSA) |> summarise(count = n())
 grouped.juv$count <- grouped.juv$count / sum(grouped.juv$count)
+grouped.juv$CRIME_PSA <- as.double(grouped.juv$CRIME_PSA)
 grouped.juv$count[is.na(grouped.juv$count)] <- 0
 names(grouped.juv) <- c("Arrest.Location.PSA", "count.juv")
-grouped.juv
 
-combined <- full_join(grouped.adult, grouped_juv, by = Arrest.Location.PSA)
+grouped.juv <- grouped.juv[!is.na(grouped.juv$Arrest.Location.PSA),]
+
+combined <- full_join(grouped.adult, grouped.juv, by = "Arrest.Location.PSA")
 combined <- data.frame(Adult = combined$count, Juv = combined$count.juv)
-combined
-combined <- combined * 1000000
-chisq.test(combined)
+
+par(cex.axis = 1.5)
+barplot(height = grouped.adult$count, names.arg = grouped.adult$Arrest.Location.PSA, las = 2, main = "Adult Arrests")
+
+# barplot(height = grouped.juv$count.juv, names.arg=grouped.juv$Arrest.Location.PSA, las = 2, main = "Juvenile Arrests")
+
+ks.test(combined$Adult, combined$Juv)
+#
 #
 #
 #
