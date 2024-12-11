d39e1e42408e61f07291c7a4570a85a79cf4dbca
Sophia Rutman <srutman02@gmail.com>
2024-12-11 12:03:13 -0500

    final push

--- juv-adult-tests/.vdoc.r
+++ juv-adult-tests/.vdoc.r
@@ -17,6 +17,7 @@ library(DescTools)
 adult <- read_csv("../2013-2023/arrests_2013-2023.csv") |> filter(Arrest.Year >= 2016)
 adult$Arrest.Date <- as.Date(adult$Arrest.Date, format = "%Y-%m-%d")
 grouped_adult <- adult |> group_by(Arrest.Date) |> summarize(count = n())
+grouped_adult <- head(grouped_adult, -1) 
 grouped_adult$count <- grouped_adult$count / sum(grouped_adult$count)
 grouped_adult
 # last date has absurd amount of arrests, on the scale of 10^8
@@ -30,6 +31,12 @@ juv$Arrest.Year <- format(juv$ARREST_DATE,"%y")
 grouped_juv <- juv |> group_by(ARREST_DATE) |> summarize(count = n())
 grouped_juv$count <- grouped_juv$count / sum(grouped_juv$count)
 barplot(height=grouped_juv$count, names.arg=grouped_juv$ARREST_DATE, main = "Juvenile Arrests")
+#
+#
+#
+grouped_juv <- juv |> group_by(ARREST_DATE) |> summarize(count = n()) |> arrange(desc(count))
+grouped_juv
+#
 #
 #
 #
@@ -47,7 +54,6 @@ barplot(height = sliced$count, names.arg = sliced$Arrest.Category, las = 2, main
 grouped.juv.unscaled <- juv |> group_by(TOP_CHARGE_DESCRIPTION) |> summarize(count = n()) |> arrange(desc(count))
 grouped.juv$count <- grouped.juv.unscaled$count / sum(grouped.juv.unscaled$count)
 sliced <- grouped.juv |> slice(0:20)
-
 #barplot(height = sliced$count, names.arg = sliced$TOP_CHARGE_DESCRIPTION, las = 2, main = "Juvenile Arrests")
 
 names(grouped.juv) <- c("Arrest.Category", "count.juv")
@@ -72,8 +78,12 @@ grouped.adult$count <- grouped.adult$count / sum(grouped.adult$count)
 grouped.adult$count[is.na(grouped.adult$count)] <- 0
 grouped_adult <- grouped.adult[!is.na(grouped.adult$Arrest.Location.PSA),]
 
-grouped.juv <- juv |> group_by(CRIME_PSA) |> summarise(count = n())
-grouped.juv$count <- grouped.juv$count / sum(grouped.juv$count)
+grouped.juv <- juv |> group_by(CRIME_PSA) |> summarise(count = n()) |> arrange(count)
+
+
+
+
+#grouped.juv$count <- grouped.juv$count / sum(grouped.juv$count)
 grouped.juv$CRIME_PSA <- as.double(grouped.juv$CRIME_PSA)
 grouped.juv$count[is.na(grouped.juv$count)] <- 0
 names(grouped.juv) <- c("Arrest.Location.PSA", "count.juv")
@@ -90,6 +100,20 @@ barplot(height = grouped.adult$count, names.arg = grouped.adult$Arrest.Location.
 
 ks.test(combined$Adult, combined$Juv)
 #
+#
+#
+
+grouped_juv <- juv |> group_by(ARREST_DATE) |> summarize(count = n()) |> arrange(desc(count))
+
+
+arrests_ts_juv <- ts(juv_data$count, 
+                 start = c(2016, 1), 
+                 frequency = 12)  # Monthly data
+# Decompose the time series
+decomposed_ts_juv <- decompose(arrests_ts_juv)
+
+# Plot decomposition
+plot(decomposed_ts_juv)
 #
 #
 #
